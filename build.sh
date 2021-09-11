6beb8263133e6143fbce4a1d95c5d310c7c8236d
sam bacha <sam@manifoldfinance.com>
2021-09-10 17:37:20 -0700

    rework build and setup

--- build.sh
+++ build.sh
@@ -1,32 +1,7 @@
 #!/bin/bash
 set -x
-
-BUILD_DATE=$(date -u +'%Y-%m-%dT%H:%M:%SZ')
-#VERSION=${GITHUB_REF#refs/tags/v}
-
-# check for rust
-cargo --version | exit 1 && echo "cargo not found"
-
-
-# make directories
-mkdir -p bin/
-mkdir -p docs/
-
-echo "Download tree-sitter.js"
-wget https://github.com/tree-sitter/tree-sitter/releases/download/v0.20.0/tree-sitter.js
-mv tree-sitter.js assets/js/tree-sitter.js
-
-npm install
-echo "Generating tree-sitter requirements..."
-npx tree-sitter-cli generate
-
-echo "Building WASM Solidity Parser..."
-npx tree-sitter-cli build-wasm
-mv tree-sitter-solidity.wasm /assets/js/tree-sitter-solidity.wasm
-chmod +x assets/js/tree-sitter-solidity.wasm
-
-sleep 1
-echo "configure LANGUAGE_BASE_URL for proper resolution"
+ls output/ || exit 1 && echo "Run Setup first!"
+rm -rf output/
 bundle exec jekyll build -d output/
-
+echo "site ready in output/ directory!"
 exit 0
